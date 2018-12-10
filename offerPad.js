ce06ba142a5883a504cc6881b84dc3b92dc85e08
Vinnie King <vinnie.king@tallwave.com>
2018-12-10 10:35:41 -0700

    first commit

---
+++ offerPad.js
@@ -0,0 +1,36 @@
+// This is after the time ran out so I don't have the exact intructions
+// in front of me, hence the names of the files might be off
+
+var fs = require('fs');
+var contents = fs.readFileSync('test.txt', 'utf8');
+
+const arr = contents.split("\n");
+
+const cppArray = [];
+const cArray = [];
+const csArray = [];
+
+arr.forEach((el) => {
+    if (el.indexOf('c_') !== -1) {
+        cArray.push(el);
+    }
+    if (el.indexOf('cs_') !== -1) {
+        csArray.push(el);
+    }
+    if (el.indexOf('cpp_') !== -1) {
+        cppArray.push(el);
+    }
+});
+
+const c_writeStream = fs.createWriteStream("c_test.txt");
+const cpp_writeStream = fs.createWriteStream("cpp_test.txt");
+const cs_writeStream = fs.createWriteStream("cs_test.txt");
+
+c_writeStream.write(cArray.join('\n'))
+cpp_writeStream.write(cppArray.join('\n'));
+cs_writeStream.write(csArray.join('\n'));
+
+c_writeStream.end()
+cpp_writeStream.end()
+cs_writeStream.end()
+
